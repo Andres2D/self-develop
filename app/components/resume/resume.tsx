8fdd4fddb17f6b90e3ed8b879c587b19a5e195bb
Andres2D <andres2d1997.1206@gmail.com>
2023-06-02 20:55:41 -0500

    Add resume page

---
+++ app/components/resume/resume.tsx
@@ -0,0 +1,10 @@
+import ContentWrapper from '../shared/content-wrapper';
+const Resume = () => {
+  return (
+    <ContentWrapper>
+      <h1>Resume creation</h1>
+    </ContentWrapper>
+  );
+};
+
+export default Resume;
--- app/routes/resume.$resumeId.tsx
+++ app/routes/resume.$resumeId.tsx
@@ -1,7 +1,17 @@
-const Resume = () => {
+import type { V2_MetaFunction } from "@remix-run/node";
+import Resume from "~/components/resume/resume";
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "Resume" },
+    { name: "description", content: "Create your resume" },
+  ];
+};
+
+const ResumePage = () => {
   return (
-    <h1>Resume creation</h1>
+    <Resume />
   )
 };
 
-export default Resume;
+export default ResumePage;
