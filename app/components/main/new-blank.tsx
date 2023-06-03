5ae8c2ba771af3c29caaf71330864df58cc20a86
Andres2D <andres2d1997.1206@gmail.com>
2023-06-02 20:33:30 -0500

    Add new blank button option

---
+++ app/components/main/new-blank.tsx
@@ -0,0 +1,11 @@
+import styles from './new-blank.module.css';
+
+export const NewBlank = () => {
+  return (
+    <div className={styles.new}>
+      <h3>Blank</h3>
+    </div>
+  );
+};
+
+export default NewBlank;
