2ef2eef5d2865a96e82894cd360a9e6568c5c412
Andres2D <andres2d1997.1206@gmail.com>
2023-06-02 20:34:28 -0500

    Add some global styles

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { V2_MetaFunction } from "@remix-run/node";
-import Main from "~/components/main";
+import Main from "~/components/main/main";
 
 export const meta: V2_MetaFunction = () => {
   return [
