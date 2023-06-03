b3c70a7d0799571adc772cd0f8f4e0e8a4b9e084
Andres2D <andres2d1997.1206@gmail.com>
2023-06-02 20:43:45 -0500

    Add redirect to new resume page

--- app/components/main/new-blank.tsx
+++ app/components/main/new-blank.tsx
@@ -1,8 +1,15 @@
+import { useNavigate } from '@remix-run/react';
 import styles from './new-blank.module.css';
 
 export const NewBlank = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/resume/123');
+  };
+
   return (
-    <div className={styles.new}>
+    <div className={styles.new} onClick={handleClick}>
       <h3>Blank</h3>
     </div>
   );
---
+++ app/routes/resume.$resumeId.tsx
@@ -0,0 +1,7 @@
+const Resume = () => {
+  return (
+    <h1>Resume creation</h1>
+  )
+};
+
+export default Resume;
