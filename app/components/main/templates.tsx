49e411828455c43e9357a7887d571c6ab0edc701
Andres2D <andres2d1997.1206@gmail.com>
2023-06-02 20:33:48 -0500

    Add new templates component

---
+++ app/components/main/templates.tsx
@@ -0,0 +1,18 @@
+import styles from './templates.module.css';
+
+const Templates = () => {
+  return (
+    <div className={styles.templates}>
+      <h1 className={styles.title}>Templates</h1>
+      <div className={styles.template}></div>
+      <div className={styles.template}></div>
+      <div className={styles.template}></div>
+      <div className={styles.template}></div>
+      <div className={styles.template}></div>
+      <div className={styles.template}></div>
+    </div>
+  )
+};
+
+export default Templates;
+
