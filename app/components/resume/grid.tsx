1f9c928fe9a43396740e317c9d33752147b77c2b
Andres2D <andres2d1997.1206@gmail.com>
2023-06-05 20:02:49 -0500

    Refactor grid page

--- app/components/resume/grid.tsx
+++ app/components/resume/grid.tsx
@@ -1,20 +1,14 @@
 import styles from './grid.module.css';
-import { VscSettings } from 'react-icons/vsc';
 
 const Grid = () => {
   return (
-    <section className={styles.resume}>
-      <nav className={styles.navigator}>
-        <button className={styles.settings}><VscSettings /></button>
-      </nav>
-      <section className={styles.grid}>
-        <div className={`${styles.profile} ${styles.section}`}>profile</div>
-        <header className={`${styles.header} ${styles.section}`}>header</header>
-        <aside className={`${styles.skills} ${styles.section}`}>skills</aside>
-        <article className={`${styles.experience} ${styles.section}`}>experience</article>
-        <aside className={`${styles.links} ${styles.section}`}>links</aside>
-        <article className={`${styles.certificates} ${styles.section}`}>certificates</article>
-      </section>
+    <section className={styles.grid}>
+      <div className={`${styles.profile} ${styles.section}`}>profile</div>
+      <header className={`${styles.header} ${styles.section}`}>header</header>
+      <aside className={`${styles.skills} ${styles.section}`}>skills</aside>
+      <article className={`${styles.experience} ${styles.section}`}>experience</article>
+      <aside className={`${styles.links} ${styles.section}`}>links</aside>
+      <article className={`${styles.certificates} ${styles.section}`}>certificates</article>
     </section>
   )
 };
