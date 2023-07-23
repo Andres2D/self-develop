90556f740fe4c28ed460e8bb562426cb56a21883
Andres2D <andres2d1997.1206@gmail.com>
2023-07-23 15:04:45 -0500

    Refactor presentation section into form

--- app/components/resume/components/grid/header.tsx
+++ app/components/resume/components/grid/header.tsx
@@ -4,11 +4,29 @@ type Props = {
   className: string;
 };
 
+const defaultDescription = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
+
 const Header = ({ className }: Props) => {
   return (
     <div className={`${className} ${styles.header}`}>
-      <h1 className={styles.title}>Andres Alcaraz</h1>
-      <h3 className={styles.role}>Software developer</h3>
+      <input 
+        className={styles.title}
+        type="text" 
+        defaultValue="Andres Alcaraz"
+      />
+      <input 
+        className={styles.role}
+        type="text"
+        defaultValue="Software developer"
+      />
+      <textarea 
+        className={styles.description}
+        name="" 
+        id=""
+        defaultValue={defaultDescription}
+        cols={10} 
+        rows={10}
+      />
     </div>
   )
 };
